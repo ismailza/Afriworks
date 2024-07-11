d3bbda49abb91362eaa3bcb00f9655f62488a215
Ismail ZAHIR <ismailza407@gmail.com>
2024-07-11 16:55:41 +0100

    setup requests

---
+++ app/Http/Requests/ServiceRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ServiceRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'service_id' => ['required', 'integer', 'exists:services,id'],
+            'description' => ['required', 'string'],
+            'date' => ['required', 'date'],
+            'name' => ['required', 'string', 'max:60'],
+            'email' => ['required', 'email', 'max:120'],
+            'phone' => ['required', 'string', 'max:20'],
+        ];
+    }
+}
