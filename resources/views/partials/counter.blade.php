a5e9d95b8ab878318981aed198e3aa1c1e02aee7
Ismail ZAHIR <ismailza407@gmail.com>
2024-07-11 17:08:56 +0100

    add counter

---
+++ resources/views/partials/counter.blade.php
@@ -0,0 +1,50 @@
+<section class="counter-area-one">
+    <div class="container">
+        <div class="row g-4 g-lg-0">
+            <div class="col-sm-6 col-lg-3">
+                <div class="counter-one-item d-flex align-items-center">
+                    <div class="icon">
+                        <img src="{{ asset('assets/img/counter/counter-1.svg') }}" alt="feature-1">
+                    </div>
+                    <div class="info">
+                        <h2 class="counter-number"><span class="odometer" data-count="200"></span>+</h2>
+                        <p>Winning award</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="counter-one-item d-flex align-items-center">
+                    <div class="icon">
+                        <img src="{{ asset('assets/img/counter/counter-2.svg') }}" alt="feature-2">
+                    </div>
+                    <div class="info">
+                        <h2 class="counter-number"><span class="odometer" data-count="550"></span>+</h2>
+                        <p>Project Done</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="counter-one-item d-flex align-items-center">
+                    <div class="icon">
+                        <img src="{{ asset('assets/img/counter/counter-3.svg') }}" alt="feature-3">
+                    </div>
+                    <div class="info">
+                        <h2 class="counter-number"><span class="odometer" data-count="100"></span>+</h2>
+                        <p>Clients Review</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3">
+                <div class="counter-one-item d-flex align-items-center">
+                    <div class="icon">
+                        <img src="{{ asset('assets/img/counter/counter-4.svg') }}" alt="feature-4">
+                    </div>
+                    <div class="info">
+                        <h2 class="counter-number"><span class="odometer" data-count="350"></span>+</h2>
+                        <p>Team Member</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
