e9d1041293fc28c8fecab5dbf5a94d52d530b06c
Ismail ZAHIR <ismailza407@gmail.com>
2024-07-11 16:47:57 +0100

    add migrations

---
+++ database/migrations/2024_07_02_170104_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Service;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('image');
+            $table->string('excerpt');
+            $table->text('description');
+            $table->foreignIdFor(Service::class)->nullable();
+            $table->date('start_date')->nullable();
+            $table->date('end_date')->nullable();
+            $table->string('url')->nullable();
+            $table->string('client')->nullable();
+            $table->string('status')->default('draft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
